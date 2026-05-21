646e7347c9d4a125c7d4f4165c8995f212eee336
AnshikaSarasawat55 <anshikasaraswat420@gmail.com>
2026-05-21 15:12:53 +0530

    Time: 2 ms (72.52%), Space: 44.8 MB (45.96%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,59 +1,21 @@
 class Solution {
      public List<String> generateParenthesis(int n) {
-    //     List<String> st=new ArrayList<>();
-    //     solve(n , 0,0,st,"");
-    //     return st;
-    // }
-    // public void solve(int n , int open, int close,List<String> st,String s){
-    //     if(open == n && close ==n){
-    //         st.add(s);
-    //         return ;
-    //     }
-    //     if(open < n){
-    //         solve(n,open+1 ,close,st,s+"(");
-    //     }
-    //     if(close< open){
-    //         solve(n,open,close+1,st,s+")");
-    //     }
-    //     return ;
-    // }
-         List<String> ans = new ArrayList<>();
-
-        StringBuilder sb = new StringBuilder();
-
-        solve(n, 0, 0, sb, ans);
-
-        return ans;
+        List<String> st=new ArrayList<>();
+        solve(n , 0,0,st,"");
+        return st;
     }
-
-    public void solve(int n, int open, int close,
-                      StringBuilder sb,
-                      List<String> ans) {
-
-        // base case
-        if (open == n && close == n) {
-            ans.add(sb.toString());
-            return;
+    public void solve(int n , int open, int close,List<String> st,String s){
+        if(open == n && close ==n){
+            st.add(s);
+            return ;
         }
-
-        // add (
-        if (open < n) {
-
-            sb.append('(');
-
-            solve(n, open + 1, close, sb, ans);
-
-            sb.deleteCharAt(sb.length() - 1); // backtrack
+        if(open < n){
+            solve(n,open+1 ,close,st,s+"(");
         }
-
-        // add )
-        if (close < open) {
-
-            sb.append(')');
-
-            solve(n, open, close + 1, sb, ans);
-
-            sb.deleteCharAt(sb.length() - 1); // backtrack
+        if(close< open){
+            solve(n,open,close+1,st,s+")");
         }
+        return ;
     }
+     
 }
